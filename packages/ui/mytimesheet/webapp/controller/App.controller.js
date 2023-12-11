11ee7ac5d9f5a216c9b208400af8ba4616f2721a
arun.krishnamoorthy@aarini.com <arun.krishnamoorthy@aarini.com>
2023-12-11 10:23:17 +0000

    [Fix]-My timesheet UI changes

--- packages/ui/mytimesheet/webapp/controller/App.controller.js
+++ packages/ui/mytimesheet/webapp/controller/App.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-  "com/ordinats/ui/mytimesheet/controller/BaseController"
+  "com/ordinats/ui/mytimesheet/controller/BaseController",
+  "sap/ui/model/json/JSONModel"
 ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (BaseController) {
+  function (BaseController,JSONModel) {
       "use strict";
 
       return BaseController.extend("com.ordinats.ui.mytimesheet.controller.App", {
@@ -12,11 +13,20 @@ sap.ui.define([
           onInit: function () {
             this._intializeViewModel();
             this.getView().getModel("view").setProperty("/sideNavigation",true);
+            let model = new JSONModel();
+                model.setData({
+                    "date" : new Date()
+                });
+                this.getView().setModel(model,"dateinput");
           },
 
           handleToggle: function() {
             let toggle = this.getView().getModel("view").getData().sideNavigation;
             this.getView().getModel("view").setProperty("/sideNavigation",!toggle);
+          },
+
+          handleOpenDatePicker: function(oEvent) {
+            this.getView().byId("idHiddenDate").openBy(oEvent.getSource().getDomRef());
           }
       });
   });
